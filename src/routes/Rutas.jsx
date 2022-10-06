58222833f218d9370202ec10d7a28fabad12ff16
arirom <arielarome15@gmail.com>
2022-10-06 12:14:56 -0300

    first commit

---
+++ src/routes/Rutas.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { 
+
+    BrowserRouter as Router,
+    Routes,
+    Route,
+ }  from "react-router-dom";
+
+import Home from '../views/home/Home';
+import Info from '../views/informacion/Info';
+import Login from '../views/login/Login'; 
+import Admin from '../views/panel/Admi';
+
+
+export default function Rutas  ()  {
+  return (
+  
+    <Router>
+      
+        <Routes>
+          <Route exact path="/Informacion" element={<Info/>}/>
+          <Route exact path="/Panel" element={<Admin/>}/>
+          <Route exact path="/Iniciosesion" element={<Login/>}/> 
+          <Route exact path="/" element={<Home/>}/>
+        </Routes>
+     
+    </Router>
+  
+
+  )
+}
+
---
+++ src/views/informacion/Info.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../../styles/Styleinfo.css";
+export default function Info () {
+  return (
+    <div> perfecto Info</div>
+  )
+}
+
